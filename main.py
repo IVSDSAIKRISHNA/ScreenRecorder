0a25bd1ac8e26fca484ce6c79797d0afd8e7e0f6
IVSDSAIKRISHNA <ivsdsaikrishna1994@gmail.com>
2022-08-25 01:59:49 +0530

    +

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #this is a screen recoder using python
 # we are gonna take a screenshot for every millisecond and then merge them all together
-
+#importing the modules 
 import cv2
 import pyautogui
 from win32api import GetSystemMetrics
@@ -11,3 +11,4 @@ width=GetSystemMetrics(0)
 height=GetSystemMetrics(1)
 
 dim=(width,height)
+
